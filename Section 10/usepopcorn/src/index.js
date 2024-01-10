758eae980251ee7364fe61bbbdd983336a218f24
Pablo Fernandez <p.fernandez@quantcast.com>
2024-01-10 02:54:12 -0300

    Finished section 10

--- Section 10/usepopcorn/src/index.js
+++ Section 10/usepopcorn/src/index.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+//import "./index.css";
+//import App from "./App";
+import StartRating from "./StartRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StartRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This is the movie rate {movieRating}</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <StartRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test></Test>
   </React.StrictMode>
 );
